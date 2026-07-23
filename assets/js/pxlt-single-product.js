a43bd6fd62f976bd3b58e9857aa8dcbf42f4245b
Salesh Kumar <narayan1691.sk@gmail.com>
2026-07-23 20:04:04 +0530

    single product ui changes

--- assets/js/pxlt-single-product.js
+++ assets/js/pxlt-single-product.js
@@ -1,4 +1,4 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
     var mediaQuery = window.matchMedia('(max-width: 768px)');
     function handleMobileViewForTab(mediaQuery) {
         var element = $('.pxlt_tab');
@@ -11,13 +11,28 @@ jQuery(document).ready(function($) {
             }
         }
     }
-    
-    function handleMobileViewForTabContent(element){
-        element.on('click', function() {
-            $(this).toggleClass('active'); 
-            $('#pxlt_tab_content').toggle(); 
+
+    function handleMobileViewForTabContent(element) {
+        element.on('click', function () {
+            $(this).toggleClass('active');
+            $('#pxlt_tab_content').toggle();
         });
     }
     handleMobileViewForTab(mediaQuery);
     mediaQuery.addEventListener('change', handleMobileViewForTab);
 });
+
+
+jQuery(function ($) {
+    var $wishlist = $('.yith-add-to-wishlist-button-block--single');
+    var $image = $('.pxlt-original-image-container');
+
+    if ($wishlist.length && $image.length) {
+        $wishlist.add($image).wrapAll('<div class="pxlt-button-wrap"></div>');
+    }
+});
+
+
+// jQuery(document).ready(function ($) {
+//     $('.single-product .pxlt-original-image-container').appendTo('.single-product .yith-add-to-wishlist-button-block');
+// });
